54775d5d3c55a088715553b78285f315edad6552
Mark Cirineo <mcirin3@uic.edu>
2024-12-30 23:20:13 -0600

    more changes

--- developer-portfolio-main/app/components/navbar.jsx
+++ developer-portfolio-main/app/components/navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
         <div className="flex flex-shrink-0 items-center">
           <Link
             href="/"
-            className=" text-[#16f2b3] text-3xl font-bold">
+            className=" text-[#42acdc] text-3xl font-bold">
             MARK CIRINEO
           </Link>
         </div>
