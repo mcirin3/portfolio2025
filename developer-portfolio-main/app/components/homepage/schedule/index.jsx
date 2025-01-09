99a8a97296418c470f7ef768e1e606647d28d147
mcirin3 <mcirin3@uic.edu>
2025-01-09 09:41:47 -0600

    fixed times

--- developer-portfolio-main/app/components/homepage/schedule/index.jsx
+++ developer-portfolio-main/app/components/homepage/schedule/index.jsx
@@ -11,9 +11,9 @@ const GlowCard = dynamic(() => import("../../helper/glow-card"), { ssr: false })
 // Mock CSV data (replace with actual CSV loading logic)
 const csvData = `Day,Time,Course,Location
 MWF, 2:00 PM - 2:50 PM, CS 442: Software Engineering II , BSB 145
-MWF, 3:30 PM - 4:45 PM,CS 422: User Interface & Design Programming, TBH 180G
-MWF, 5:00 PM - 6:15 PM,CS 494: Mobile Robotics for CS, TBH 180G
-T, 11:00 AM - 1:50 PM, CS 211 Lab Proctoring, SELE 2263
+MWF, 3:30 PM - 4:45 PM, CS 422: User Interface & Design Programming, TBH 180G
+MW, 5:00 PM - 6:15 PM, CS 494: Mobile Robotics for CS, TBH 180G
+T, 10:00 AM - 1:50 PM, CS 211 Lab Proctoring, SELE 2263
 W, 11:15 AM - 1:15 PM,CS 211 Office Hours, CS Lounge
 S, 8:00 - 9:00 PM,CS 211 Office Hours, (ONLINE via Zoom)`;
 
