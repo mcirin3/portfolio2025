88c94ff3776478974e227a2cac5e1617e090ffd5
mcirin3 <mcirin3@uic.edu>
2025-01-12 18:24:06 -0600

    updated to use web3 for contact

--- developer-portfolio-main/app/components/homepage/contact/contact-form.jsx
+++ developer-portfolio-main/app/components/homepage/contact/contact-form.jsx
@@ -1,7 +1,6 @@
 "use client";
 // @flow strict
 import { isValidEmail } from "@/utils/check-email";
-import axios from "axios";
 import { useState } from "react";
 import { TbMailForward } from "react-icons/tb";
 import { toast } from "react-toastify";
@@ -31,33 +30,46 @@ function ContactForm() {
       return;
     } else {
       setError({ ...error, required: false });
-    };
+    }
 
     try {
       setIsLoading(true);
-      const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
-        userInput
-      );
-
-      toast.success("Message sent successfully!");
-      setUserInput({
-        name: "",
-        email: "",
-        message: "",
+      const res = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          access_key: "f0794fae-7455-47ea-a71d-d47d766c3b47",
+          ...userInput,
+        }),
       });
+
+      if (res.ok) {
+        toast.success("Message sent successfully!");
+        setUserInput({
+          name: "",
+          email: "",
+          message: "",
+        });
+      } else {
+        const errorData = await res.json();
+        toast.error(errorData.message || "Something went wrong!");
+      }
     } catch (error) {
-      toast.error(error?.response?.data?.message);
+      toast.error("Failed to send the message. Please try again.");
     } finally {
       setIsLoading(false);
-    };
+    }
   };
 
   return (
     <div>
       <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase">Contact with me</p>
       <div className="max-w-3xl text-white rounded-lg border border-[#464c6a] p-3 lg:p-5">
-        <p className="text-sm text-[#d3d8e8]">{"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}</p>
+        <p className="text-sm text-[#d3d8e8]">
+          {"If you have any questions or concerns, please don't hesitate to contact me. I am open to any work opportunities that align with my skills and interests."}
+        </p>
         <div className="mt-6 flex flex-col gap-4">
           <div className="flex flex-col gap-2">
             <label className="text-base">Your Name: </label>
@@ -103,29 +115,29 @@ function ContactForm() {
             />
           </div>
           <div className="flex flex-col items-center gap-3">
-            {error.required && <p className="text-sm text-red-400">
-              All fiels are required!
-            </p>}
+            {error.required && (
+              <p className="text-sm text-red-400">All fields are required!</p>
+            )}
             <button
               className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-5 md:px-12 py-2.5 md:py-3 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
               role="button"
               onClick={handleSendMail}
               disabled={isLoading}
             >
-              {
-                isLoading ?
-                <span>Sending Message...</span>:
+              {isLoading ? (
+                <span>Sending Message...</span>
+              ) : (
                 <span className="flex items-center gap-1">
                   Send Message
                   <TbMailForward size={20} />
                 </span>
-              }
+              )}
             </button>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default ContactForm;
