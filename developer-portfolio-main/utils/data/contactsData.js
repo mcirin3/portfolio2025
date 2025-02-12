3b7b24824eef120ceec0b2a3340b00304988b96c
Mark Cirineo <mcirin3@uic.edu>
2025-02-12 08:55:13 -0600

    link

--- developer-portfolio-main/utils/data/contactsData.js
+++ developer-portfolio-main/utils/data/contactsData.js
@@ -4,7 +4,7 @@ export const contactsData = {
     address: '851 S Morgan St ',
     github: 'https://github.com/mcirin3',
     facebook: 'https://www.facebook.com/mcirineo9/',
-    linkedIn: 'https://www.linkedin.com/in/markcirineo-22365/',
+    linkedIn: 'https://www.linkedin.com/in/mark-cirineo-65248b175/',
     twitter: 'https://twitter.com/mcirineo9',
     stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
     devUsername: "mcirineo9"
